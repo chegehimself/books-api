62729f576f6a1016c489f5cf35cd98749d4c8692
james-chege <jymeschege@gmail.com>
2018-10-29 10:07:51 +0300

    feat(login): Build node with express api - powered the api with authentication

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+import auth from './routes/auth';
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.json());
+
+// found out that the following is deprecated
+// mongoose.connect('mongodb://localhost/bookworm',  { useMongoClient: true });
+
+// Use the following url parser
+mongoose.connect(process.env.MONGODB_URL,  { useNewUrlParser: true });
+
+app.use("/api/auth", auth);
+
+app.post("/api/auth", (req, res) => {
+    res.status(400).json({ errors: {global: "Invalid credentials"}});
+});
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(8080, () => console.log('running on localhost:8080'));
